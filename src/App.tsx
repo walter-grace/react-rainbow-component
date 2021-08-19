ad96b6fb824860df1fc31b45f56dadd5075c3936
walter-grace <nico.zahniser@gmail.com>
2021-08-18 20:33:36 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import { RainbowButton } from '@rainbow-me/rainbow-button';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>
+        Connect for an NFT drop
+      </h4>
+      <RainbowButton
+      chainId={1}
+      connectorOptions={{ bridge: 'https://bridge.walletconnect.org' }}
+      onConnectorInitialized={(connector) => console.log(connector)}
+    />
     </div>
   );
 }
