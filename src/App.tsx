02f46a95353aeaebb34c88f141f0cb7c6f25dd96
walter-grace <nico.zahniser@gmail.com>
2021-08-19 16:00:14 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import './App.css';
-import WalletConnect from "@walletconnect/client";
-import QRCodeModal from "@walletconnect/qrcode-modal";
 import { RainbowButton } from '@rainbow-me/rainbow-button';
 
+function UserGreeting(props: any) {
+  return <h1>Welcome back!</h1>;
+}
 
-function App() {
-  return (
-    <div className="App">
-      <h4>
-        Connect for an NFT drop
-      </h4>
+function GuestGreeting(_props: any) {
+  return <div>
+    <h1>Please sign up, to get an NFT</h1>
       <RainbowButton
       chainId={1}
       connectorOptions={{ bridge: 'https://bridge.walletconnect.org' }}
       onConnectorInitialized={(connector) => console.log(connector)}
-    />
+    />;
+    </div>
+}
+
+function Greeting(props: { isLoggedIn: any; }) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+  // Try changing to isLoggedIn={true}:
+  
+  function App() {
+    return (
+      <div className="App">
+      <Greeting isLoggedIn={false} />
+     
+
     </div>
   );
 }
